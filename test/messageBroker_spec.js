74ea9eb3f426d59c38aca14535c90c8f8a7dd653
Andrei Scripniciuc <scripni@users.noreply.github.com>
2015-07-28 21:12:00 +0100

    tidy up

---
+++ test/messageBroker_spec.js
@@ -0,0 +1,64 @@
+var assert = require("assert");
+var MessageBroker = require("../lib/messageBroker");
+
+describe("a message broker", function() {
+	describe("receive an authentication response", function() {
+
+	});
+
+	describe("receive a welcome message", function() {
+
+	});
+
+	describe("receive a game seek message", function() {
+
+	});
+
+	describe("receive a game start message", function() {
+
+	});
+
+	describe("receive a game move message", function() {
+
+	});
+
+	describe("receive a game end message", function() {
+
+	});
+
+	describe("receive a global chat message", function() {
+
+	});
+
+	describe("receive a private chat message", function() {
+
+	});
+
+	describe("receive an unknown message", function() {
+
+	});
+
+	describe("send an authentication message", function() {
+
+	});
+
+	describe("send a game seek message", function() {
+
+	});
+
+	describe("send a global chat message", function() {
+
+	});
+
+	describe("send a private chat message", function() {
+
+	});
+
+	describe("send a message when the receiver is unavailable", function() {
+
+	});
+
+	describe("send a null message", function() {
+
+	});
+});
--- test/telnetClient_spec.js
+++ test/telnetClient_spec.js
@@ -1,65 +1,49 @@
 var assert = require("assert");
 var TelnetClient = require("../lib/telnetClient");
 
-describe("a telnet client", function() {
+describe("a telnet client to freechess.org", function() {
 
-	describe("when the network is flowing like Viktor's tears when he loses at FIFA", function() {
-
+	// when the network is flowing like Viktor's tears when he loses at FIFA
+	describe("read an anonymous authentication fragment", function() {
 		var telnetClient = {};
-		var message = "";
-
 		before(function() {
-
 			telnetClient = new TelnetClient({
-
+				connect: function(port, hostname, onDone) {
+					onDone();
+				},
+				on: function(event, onNext) {
+					if (event === "data") {
+						onNext("");
+					}
+				}
 			});
-
 		});
 
-		function receivedMessage(msg) {
-
-			message = msg;
-
-		}
-
 		it("is initialized in a disconnected state", function() {
-
 			assert(telnetClient.getIsConnected() === false);
-
 		});
 
 		it("can initiate the connection with the server", function(onDone) {
-
 			this.timeout(5000);
 			telnetClient.connectAsync(function() {
-
 				assert(telnetClient.getIsConnected());
 				onDone();
-
 			});
 		});
 
 		it("receives a message", function(onDone) {
-
 			this.timeout(5000);
 			telnetClient.beginReadAsync(function(message) {
-
 				telnetClient.destroy();
 				assert(message.length > 0);
 				onDone();
-
 			});
-
 		});
 
 		after(function() {
-
 			telnetClient.destroy();
 			assert(telnetClient.getIsConnected() === false);
-
 		});
-
 	});
-
 });
 
