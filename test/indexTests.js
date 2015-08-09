e8eadb2c7259d8ef2738c551aa4f64d5594c46d4
Andrei Scripniciuc <scripni@users.noreply.github.com>
2015-08-09 08:09:56 +0100

    Cleanup

--- test/indexTests.js
+++
@@ -1,8 +0,0 @@
-var assert = require("assert");
-var FicsUser = require("../index").ficsUser;
-
-describe("network module", function() {
-	describe("exports", function() {
-		it("exports a message broker");
-	});
-});
--- test/models/ficsUser_spec.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-var FicsUser = require("../../lib/models/ficsUser");
-
-describe("A FICS user", function() {
-	describe("A successful anonymous connection", function() {
-		it("signs in receiving a Guest**** username");
-		it("signs out if the connection is terminated");
-	});
-});
